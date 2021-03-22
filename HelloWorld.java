a81f667088bddf60b8f8801b2fff45013a1a7477
anon-ds <rhythm.kumar2@gmail.com>
2021-03-22 16:42:13 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,9 @@ class HelloWorld{
 
   public static void main(String []args){
     System.out.println("Hello World");
+    System.out.println(add(1,2));
+  }
+  public static int add(int num1,int num2){
+    return num1+num2;
   }
 }
